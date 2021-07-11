6c978f23cf5edcfc3623c0818bff62b2d3f3439c
disco0 <disco0@users.noreply.github.com>
2021-07-10 20:45:50 -0700

    remove unnecessary ts sources

--- src/options-spec.ts
+++
@@ -1,4 +0,0 @@
-import options from './options'
-
-console.log(`Parsed options:`)
-console.dir(Object.fromEntries(Object.entries(options)), {depth: 5, showHidden: true, colors: true, getters: true  })
